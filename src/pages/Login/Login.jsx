bb72f0340532a777351ee879d21c44d9970ad45e
Jonathan Martinez <jonathanmtzcon@gmail.com>
2025-04-23 13:01:37 -0600

    Login mobile styling done

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,5 +1,36 @@
+import { NavLink } from "react-router-dom"
+import { FaBookOpen } from "react-icons/fa6";
+import "./Login.css"
+
 export default function Login(){
+
+    function onClick(e){
+        e.preventDefault()
+    }
+
     return (
-        <h1>Login</h1>
+        <>
+            <main className="login-main">
+                <span className="logo login-logo">Formalize</span>
+                <div className="form-container">
+                    <form>
+                        <h1 className="login-title">Welcome back!</h1>
+                        <h2 className="login-subtitle">Please log in to continue.</h2>
+                        <div className="input-container">
+                            <label htmlFor="email">Email</label>
+                            <input type="email" name="email" id="email"/>
+                            <label htmlFor="password">Password</label>
+                            <input type="password" name="password" id="password"/>
+                        </div>
+                        <button onClick={onClick} className="login-button">Enter Formalize</button>
+                        <span className="register-span">
+                            Don't have an account? 
+                            <NavLink to="/register" className="register-link">Register here.</NavLink>
+                        </span>
+                        <FaBookOpen className="top-left-icon" size={50}/>
+                    </form>
+                </div>
+            </main>
+        </>
     )
 }
