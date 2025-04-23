b40792beea522b4de90b0594d21f4178fd514ffe
Jonathan Martinez <jonathanmtzcon@gmail.com>
2025-04-23 11:21:33 -0600

    Added login link

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Layout from './pages/Layout'
+import Login from './pages/Login/Login'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route path='login' element={<Login/>}/>
-        <Route path='register' element={<Register/>}/> */}
+        <Route path='login' element={<Login/>}/>
+        {/* <Route path='register' element={<Register/>}/> */}
         <Route element={<Layout/>}>
           <Route path='/' element={<Home/>}/>
         </Route>
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,10 +1,13 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, NavLink } from "react-router-dom"
 import "./Layout.css"
 
 export default function Layout(){
     return(
         <>
-            <header><span className="logo">Formalize</span></header>
+            <header>
+                <span className="logo">Formalize</span>
+                <NavLink to="/login" className="login-link">Login</NavLink>
+            </header>
             <main>
                 <Outlet/>
             </main>
