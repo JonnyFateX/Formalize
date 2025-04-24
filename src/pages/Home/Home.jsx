40146a1a79a1f57acb7d6ef89a8f98e92b9558a9
Jonathan Martinez <jonathanmtzcon@gmail.com>
2025-04-24 11:37:09 -0600

    Home template selection added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,33 @@
+import { FaQuestion, FaCalendar, FaUser, FaLightbulb, FaPoll } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
+import './Home.css'
+
 export default function Home(){
     return(
-        <h1>Home</h1>
+        <div className="template-selection-container">
+            <h1 className="template-selection-title">Let's create a form</h1>
+            <div className="templates">
+                <div className="template-container">
+                    <h2 className="template-title">Single question</h2>
+                    <NavLink className="template-link"><FaQuestion className="template-icon"/></NavLink>
+                </div>
+                <div className="template-container">
+                    <h2 className="template-title">Assistance confirmation</h2>
+                    <NavLink className="template-link"><FaCalendar className="template-icon"/></NavLink>
+                </div>
+                <div className="template-container">
+                    <h2 className="template-title">Contact Data</h2>
+                    <NavLink className="template-link"><FaUser className="template-icon"/></NavLink>
+                </div>
+                <div className="template-container">
+                    <h2 className="template-title">Quiz</h2>
+                    <NavLink className="template-link"><FaLightbulb className="template-icon"/></NavLink>
+                </div>
+                <div className="template-container">
+                    <h2 className="template-title">Poll</h2>
+                    <NavLink className="template-link"><FaPoll className="template-icon"/></NavLink>
+                </div>
+            </div>
+        </div>
     )
 }
